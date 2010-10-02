feb4bf9441690d7fe4d6c06546eb44bb91b8af0b
robgleeson <rob@flowof.info>
2010-10-02 01:02:19 +0100

    Refactor #authors on the Grities module.

--- lib/Grities.rb
+++ lib/Grities.rb
@@ -2,49 +2,37 @@ module Grities
 
   # TODO:
   # Document me
-  # Refactor me
-  def authors(repo, name)
-    r     = {}
-    ret   = []
-    commits = Grit::Commit.find_all(repo, nil)
-    commits.each do |commit|
-      commit.tree.trees.each do |tree|
-        r.merge!(traverse_tree(tree))
-      end
-
-      r.each do |location, blobs|
+  # Refactor me more
+  def authors(repo, filename)
+    ret = []
+    all_commits = Grit::Commit.find_all(repo, nil)
+    all_commits.each do |single_commit|
+      tree = traverse_tree(single_commit.tree)
+      tree.values.each do |blobs|
         blobs.each do |blob|
-          if blob.name == name
-            ret << { :name  => commit.author.name, 
-                     :email => Digest::MD5.hexdigest(commit.author.email) }
+          if blob.name == filename
+            ret << { :name  => single_commit.author.name,
+                     :email => single_commit.author.email }
           end
         end
       end
-
-      commit.tree.blobs.each do |blob|
-        if blob.name == name
-          ret << { :name => commit.author.name, 
-                   :email => Digest::MD5.hexdigest(commit.author.email) }
-        end
-      end
     end
-    ret.uniq!
+    ret = ret.uniq
   end 
 
   # TODO: 
   # Document me.
   def traverse_tree(tree, name = "", contents = {})
     if tree.trees.empty?
-      name << tree.name
+      name << tree.name.to_s
       contents[name] = tree.blobs
       name.clear
     else
-      tree.name.nil? ? (contents['/'] = tree.blobs) : (name << tree.name)
+      tree.name.nil? ? (contents['/'] = tree.blobs) : (name << tree.name + '/')
       tree.trees.each do |tree|
         traverse_tree(tree, name, contents)
       end
     end
-
     contents
   end
 
